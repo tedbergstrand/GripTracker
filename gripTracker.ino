94a5aef24252823bbaa8936051b98732fb4b089e
tedbergstrand <113138359+tedbergstrand@users.noreply.github.com>
2023-12-10 00:41:00 -0500

    Update gripTracker.ino

--- gripTracker.ino
+++ gripTracker.ino
@@ -33,12 +33,15 @@ const float forceThreshold = 2.5; // Threshold for force
 const int consecutiveReadingsThreshold = 5; // Number of consecutive readings to start logging
 static int consecutiveAboveThresholdCount = 0; // Counter for consecutive readings above threshold
 float lastWeight = 0;
+float lastTwoWeights[2] = {0, 0}; // Array to store the last two weights
+float lastTwoTimestamps[2] = {0, 0}; // Array to store the timestamps of the last two weights
+
 
 
 // Setup
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("GripTracker");
 
   // Set up as an Access Point
@@ -93,7 +96,7 @@ void setup() {
   server.on("/tare", HTTP_GET, handleTare);
   server.on("/forceData", HTTP_GET, handleForceData);
   server.on("/listCSV", HTTP_GET, handleListCSV);
-  server.on("/getHangSummary", HTTP_GET, handleGetHangSummary);
+//  server.on("/getHangSummary", HTTP_GET, handleGetHangSummary);
   server.on("/getRawData", HTTP_GET, handleGetRawData);
   server.on("/timers.html", HTTP_GET, handleTimers);
   server.on("/resume", HTTP_GET, handleResume);
@@ -215,24 +218,6 @@ void handleDataView() {
   }
 }
 
-void handleGetHangSummary() {
-  String fileName = "/" + server.arg("file");
-  Serial.println("Requested file: " + fileName); // Debugging line
-
-  if (SPIFFS.exists(fileName)) {
-    File file = SPIFFS.open(fileName, "r");
-    if (!file) {
-      server.send(500, "text/plain", "Error opening file: " + fileName);
-      return;
-    }
-    String summary = calculateHangSummary(file);
-    file.close();
-    server.send(200, "text/plain", summary);
-  } else {
-    server.send(404, "text/plain", "File not found");
-  }
-}
-
 void handleTimers() {
   if(SPIFFS.exists("/timers.html")) {
     File file = SPIFFS.open("/timers.html", "r");
@@ -241,22 +226,6 @@ void handleTimers() {
   } else {
     server.send(404, "text/plain", "404: Not Found");
   }
-}
-
-void handleGetTimerSettings() {
-  String protocol = server.arg("protocol");
-
-  int repAmount, repTime, repRest, setAmount, setRest;
-  if (protocol == "maxHangs") {
-    repAmount = 1; repTime = 8; repRest = -1; setAmount = 6; setRest = 180;
-  } else if (protocol == "noHang") {
-    repAmount = 12; repTime = 10; repRest = 50; setAmount = 1; setRest = -1;
-  } else { // Default to Repeaters
-    repAmount = 6; repTime = 7; repRest = 3; setAmount = 6; setRest = 180;
-  }
-
-  String jsonResponse = "{\"repAmount\": " + String(repAmount) + ", \"repTime\": " + String(repTime) + ", \"repRest\": " + String(repRest) + ", \"setAmount\": " + String(setAmount) + ", \"setRest\": " + String(setRest) + "}";
-  server.send(200, "application/json", jsonResponse);
 }
 
 void handleListCSV() {
@@ -353,96 +322,15 @@ String createNewFile() {
   }
 }
 
-String calculateHangSummary(File &file) {
-  // Start the HTML table with headers
-  String hangSummary = "<table id='hangAnalysisTable' border='1'><tr><th>Hang</th><th>Duration (s)</th><th>Peak Force (lbs)</th><th>Average Force (lbs)</th><th>Std Dev (lbs)</th></tr>";
-
-  // Initialize variables for hang data analysis
-  int hangCount = 1; // Start hang counter at 1
-  float peakForce = 0.0, totalForce = 0.0, avgForce = 0.0, hangDuration = 0.0, stdDev = 0.0;
-  int countReadings = 0;
-  bool newHang = true;
-  float startTime = 0.0, endTime = 0.0, currentTime = 0.0;
-  bool hangInProgress = false;
-  std::vector<float> forces;
-
-  // Read each line in the file
-  while (file.available()) {
-    String line = file.readStringUntil('\n');
-    int commaIndex = line.indexOf(',');
-    String timeString = line.substring(0, commaIndex);
-    String forceString = line.substring(commaIndex + 1);
-
-    // Process valid data entries
-    if (forceString != "NaN" && timeString != "0") {
-      float force = forceString.toFloat();
-      currentTime = timeString.toFloat();
-
-      // Start a new hang or continue the current hang
-      if (newHang) {
-        // If a hang is already in progress, calculate summary for it
-        if (hangInProgress) {
-          hangDuration = endTime - startTime;
-          avgForce = totalForce / countReadings;
-          stdDev = calculateStdDev(forces, avgForce);
-          hangSummary += "<tr><td>" + String(hangCount) + "</td><td>" + String(hangDuration, 2) + "</td><td>" + String(peakForce) + "</td><td>" + String(avgForce) + "</td><td>" + String(stdDev) + "</td></tr>";
-          hangCount++;
-          forces.clear();
-        }
-
-        // Initialize new hang
-        startTime = currentTime;
-        peakForce = force;
-        totalForce = force;
-        countReadings = 1;
-        newHang = false;
-        hangInProgress = true;
-        forces.push_back(force);
-      } else {
-        // Continue current hang
-        peakForce = max(peakForce, force);
-        totalForce += force;
-        countReadings++;
-        forces.push_back(force);
-      }
-
-      endTime = currentTime;
-    } else {
-      // Mark the end of a hang
-      newHang = true;
-    }
-  }
-
-  // Add summary for the last hang, if it was in progress
-  if (hangInProgress) {
-    hangDuration = endTime - startTime;
-    avgForce = totalForce / countReadings;
-    stdDev = calculateStdDev(forces, avgForce);
-    hangSummary += "<tr><td>" + String(hangCount) + "</td><td>" + String(hangDuration, 2) + "</td><td>" + String(peakForce) + "</td><td>" + String(avgForce) + "</td><td>" + String(stdDev) + "</td></tr>";
-  }
-
-  // Close the HTML table
-  hangSummary += "</table>";
-  return hangSummary;
-}
-
-float calculateStdDev(const std::vector<float>& values, float mean) {
-    float sum = 0.0;
-    for (float value : values) {
-        sum += pow(value - mean, 2);
-    }
-    return sqrt(sum / values.size());
-}
-
-void logWeight(float weight) {
-    float timestampInSeconds = millis() / 1000.0; // Current time
+void logWeight(float weight, float timestamp) {
     if (!hangInProgress) {
         hangInProgress = true;
-        currentHang.startTime = timestampInSeconds;
+        currentHang.startTime = timestamp;
         currentHang.dataLines.clear();
     }
-    currentHang.dataLines.push_back(String(timestampInSeconds, 2) + "," + String(weight));
+    currentHang.dataLines.push_back(String(timestamp - currentHang.startTime, 2) + "," + String(weight));
 }
+
 
 void markNewHangSession() {
     if (hangInProgress) {
@@ -468,38 +356,39 @@ void markNewHangSession() {
 // Loop Function
 
 void loop() {
-    scale.set_scale(-4200.00);
     float weight = scale.get_units() * -1;
+    float currentTimestamp = millis() / 1000.0;
 
-    // Use the direct reading without filtering
-    Serial.print("Reading: ");
-    Serial.print(weight, 1);
-    Serial.print(" lbs");
-    Serial.println();
+    // Update the readings
+    lastTwoWeights[0] = lastTwoWeights[1];
+    lastTwoTimestamps[0] = lastTwoTimestamps[1];
+    lastTwoWeights[1] = weight;
+    lastTwoTimestamps[1] = currentTimestamp;
 
-    // Logic for handling the force reading
-    if (abs(weight) > forceThreshold) {
-        if (!hangInProgress) {
-            // Start tracking a new hang
-            logWeight(weight);
-        } else {
-            // Continue tracking the current hang
-            logWeight(weight);
-        }
-    } else {
-        if (hangInProgress) {
+    // Check for hang session start
+    if ((abs(lastTwoWeights[0]) > forceThreshold || abs(lastTwoWeights[1]) > forceThreshold) && !hangInProgress) {
+        hangInProgress = true;
+        currentHang.startTime = lastTwoTimestamps[0];
+        currentHang.dataLines.clear();
+        logWeight(lastTwoWeights[0], lastTwoTimestamps[0]);
+    }
+    
+    // Continue logging if in a hang session
+    if (hangInProgress) {
+        logWeight(weight, currentTimestamp);
+
+        // Check for hang session end
+        if (abs(weight) <= forceThreshold) {
             float currentEndTime = millis() / 1000.0;
-            if (currentEndTime - currentHang.startTime < 1.0) {
-                // Hang duration is too short, reset the current hang data
-                currentHang.dataLines.clear();
-            } else {
-                // End the current hang session if it's long enough
+            if (currentEndTime - currentHang.startTime >= 1.0) {
                 markNewHangSession();
+            } else {
+                currentHang.dataLines.clear();
             }
             hangInProgress = false;
         }
     }
 
     server.handleClient();
-    delay(100);
+    delay(10);
 }
